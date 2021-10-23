091d8890db9830b3c3e7b810870258d36e1726b3
Bendjelloul Noureddine <bendjelloulnoureddine@gmail.com>
2021-10-23 07:48:11 +0100

    [main]: Update fields to blank & null

--- product/models.py
+++ product/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 class Category(models.Model):
-    name            = models.CharField(max_length=255)
+    name            = models.CharField(max_length=255,blank=True,null=True)
 
     def __str__(self):
         return f'{self.name}'
 
 
 class Product(models.Model):
-    name            = models.CharField(max_length=200)
+    name            = models.CharField(max_length=200,blank=True,null=True)
     buyingPrice     = models.FloatField(blank=True,null=True)
-    sellingPrice    = models.FloatField()
-    quantity        = models.IntegerField()
-    description     = models.TextField()
-    image           = models.ImageField()
+    sellingPrice    = models.FloatField(blank=True,null=True)
+    quantity        = models.IntegerField(blank=True,null=True)
+    description     = models.TextField(blank=True,null=True)
+    image           = models.ImageField(blank=True,null=True)
     
     category        = models.ForeignKey('product.Category', on_delete=models.CASCADE)
 
