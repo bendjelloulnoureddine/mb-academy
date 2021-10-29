1a3261665c46a77f15ff31054fa78e6e1939e2c3
Bendjelloul Noureddine <bendjelloulnoureddine@gmail.com>
2021-10-29 11:26:41 +0100

    [main]: Update Admin Panel

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Product, Category 
 
-admin.site.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'category__name']
+    list_display = ['name', 'buyingPrice', 'sellingPrice', 'quantity']
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductModelAdmin)
 admin.site.register(Category)
 
